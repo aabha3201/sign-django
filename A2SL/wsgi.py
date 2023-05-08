8a25ddcee55da3afd29a1548f1fd21f1922b54d2
aabha3201 <70412921+aabha3201@users.noreply.github.com>
2023-05-08 19:06:03 +0530

    first commit

---
+++ A2SL/wsgi.py
@@ -0,0 +1,12 @@
+"""
+WSGI config for A2SL project.
+web server gateway interface
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A2SL.settings')
+
+application = get_wsgi_application()
